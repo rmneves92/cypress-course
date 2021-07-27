c86682f24f29487e6840943c4231e546a14e34db
Rafael Neves <rafa.mneves@hotmail.com>
2021-07-27 09:03:13 -0300

    add asserts

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+it('A external test...', () => {});
+
+describe('Should group tests...', () => {
+  describe('Should group more specific tests...', () => {
+    it('A specific test...', () => {});
+  });
+
+  describe('Should group more specific tests...', () => {
+    it('A specific test...', () => {});
+  });
+
+  it('A internal test...', () => {});
+});
