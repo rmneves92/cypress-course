84306cbfc52a4b4f20df194d9b9e7a2eedb3e3ae
Rafael Neves <rafa.mneves@hotmail.com>
2021-07-29 20:16:50 -0300

    add helpers

--- cypress/integration/basic.spec.js
+++ cypress/integration/basic.spec.js
@@ -12,12 +12,23 @@ describe('Cypress basics', () => {
     cy.title().should('be.equal', 'Campo de Treinamento');
     cy.title().should('contain', 'Campo');
 
-    cy.title()
-      .should('be.equal', 'Campo de Treinamento')
-      .and('contain', 'Campo de Treinamento');
+    cy.title().should('be.equal', 'Campo de Treinamento').and('contain', 'Campo de Treinamento');
 
-    cy.title().should((title) => {
+    let syncTitle;
+    cy.title().then((title) => {
       console.log(title);
+
+      cy.get('#formNome').type(title);
+
+      syncTitle = title;
+    });
+
+    cy.get('[data-cy=dataSobrenome]').then(($el) => {
+      $el.val(syncTitle);
+    });
+
+    cy.get('#elementosForm\\:sugestoes').then(($el) => {
+      cy.wrap($el).type(syncTitle);
     });
   });
 
--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -30,17 +30,11 @@ describe('Work with basic elements', () => {
     cy.get('#formNome').type('Cypress Test');
     cy.get('#formNome').should('have.value', 'Cypress Test');
 
-    cy.get('#elementosForm\\:sugestoes')
-      .type('textarea')
-      .should('have.value', 'textarea');
+    cy.get('#elementosForm\\:sugestoes').type('textarea').should('have.value', 'textarea');
 
-    cy.get(
-      '#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input'
-    ).type('???');
+    cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input').type('???');
 
-    cy.get('[data-cy=dataSobrenome]')
-      .type('Teste12345{backspace}{backspace}')
-      .should('have.value', 'Teste123');
+    cy.get('[data-cy=dataSobrenome]').type('Teste12345{backspace}{backspace}').should('have.value', 'Teste123');
 
     cy.get('#elementosForm\\:sugestoes')
       .clear()
@@ -66,21 +60,31 @@ describe('Work with basic elements', () => {
     cy.get('#formComidaVegetariana').should('be.checked');
   });
 
-  it('Combo', () => {
-    cy.get('[data-test=dataEscolaridade]')
-      .select('2o grau completo')
-      .should('have.value', '2graucomp');
+  it.only('Combo', () => {
+    cy.get('[data-test=dataEscolaridade]').select('2o grau completo').should('have.value', '2graucomp');
 
-    cy.get('[data-test=dataEscolaridade]')
-      .select('1graucomp')
-      .should('have.value', '1graucomp');
+    cy.get('[data-test=dataEscolaridade]').select('1graucomp').should('have.value', '1graucomp');
 
-    // TODO: Validar as opções do combo
+    cy.get('[data-test=dataEscolaridade] option').should('have.length', 8);
+
+    cy.get('[data-test=dataEscolaridade] option').then(($arr) => {
+      const values = [];
+      $arr.each(function () {
+        values.push(this.innerHTML);
+      });
+
+      expect(values).to.include.members(['Superior', 'Mestrado']);
+    });
   });
 
-  it('Combo multiplo', () => {
+  it.only('Combo multiplo', () => {
     cy.get('[data-testid=dataEsportes]').select(['natacao', 'Corrida', 'nada']);
 
-    // TODO: Validar as opções do combo multiplo
+    cy.get('[data-testid=dataEsportes]').then(($el) => {
+      expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada']);
+      expect($el.val()).to.have.length(3);
+    });
+
+    cy.get('[data-testid=dataEsportes]').invoke('val').should('eql', ['natacao', 'Corrida', 'nada']);
   });
 });
